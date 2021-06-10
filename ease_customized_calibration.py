5b4f45cff514ded7ef224baf8069f43a4bf8b053
anonzebra <lwilsson@gmail.com>
2021-06-10 16:30:55 +0200

    (not tested/debugged yet - must be done at office) add crude automation of calibration for first calibration run

--- ease_customized_calibration.py
+++ ease_customized_calibration.py
@@ -39,13 +39,47 @@ shrink_sound = sound.Sound(SHRINK_SOUND_PATH)
 # setup the attention grabber during adjusting the participant's position
 grabber = visual.MovieStim3(win, "infant/seal-clip.mp4")
 
+def main_calibration(
+    self,
+    _focus_time=0.5,
+    collect_key='space',
+    exit_key='return'
+    ):
+    # run automated procedure if it hasn't already been run
+    if not hasattr(self, 'automated_calib_done'):
+        automated_calibration(
+            self,
+            _focus_time,
+            collect_key,
+            exit_key
+        )
+        # add attribute to 'self', ie the TobiiInfantController
+        # instance, to indicate that automated calibration
+        # has been run
+        # (any additional calibrations are to be done 'manually')
+        self.automated_calib_done = True
+    else:
+        manual_calibration(
+            self,
+            _focus_time,
+            collect_key,
+            exit_key
+        )
 
 # create a customized calibration procedure with sound
 # code snippets copied from _update_calibration_infant()
-def customized_update_calibration(self,
-                                  _focus_time=0.5,
-                                  collect_key='space',
-                                  exit_key='return'):
+def manual_calibration(
+    self,
+    _focus_time=0.5,
+    collect_key='space',
+    exit_key='return'
+    ):
+    """
+    A customized manual (see automated version below)
+    calibration procedure which uses shrink/grow sounds.
+    Partly uses code copied from the psychopy_tobii_infant 
+    package's demos.
+    """
     # boolean indicating if calibration data are to be collected
     # when target is shrunk to close to minimum size
     collect_when_shrunk = False
@@ -95,6 +129,96 @@ def customized_update_calibration(self,
             self.targets[current_point_index].draw()
             
             # handle playing sounds if target started growing/shrinking
+            if  vbcx is not None:
+                target_is_growing = previous_frame_width < newsize[0]
+                if target_is_growing and allow_grow_sound:
+                    grow_sound.play()
+                    shrink_sound.stop()
+                    allow_grow_sound = False
+                    allow_shrink_sound = True
+                elif not target_is_growing and allow_shrink_sound:
+                    shrink_sound.play()
+                    grow_sound.stop()
+                    allow_shrink_sound = False
+                    allow_grow_sound = True
+            previous_frame_width = newsize[0]
+        
+        # get current target width 
+        target_curr_width = self.targets[current_point_index].size[0]
+        if collect_when_shrunk:
+            # check if the current target width is less than twice
+            # as large as the minimum possible width
+            if target_curr_width < (2 * target_min_width):
+                # allow the participant to focus
+                core.wait(_focus_time)
+                # collect calibration data
+                self._collect_calibration_data(
+                    self.original_calibration_points[current_point_index]
+                )
+                # reset calibration point index and calibration-related
+                # boolean
+                current_point_index = -1
+                collect_when_shrunk = False
+        self.win.flip()
+
+def automated_calibration(
+    self,
+    _focus_time=0.5,
+    collect_key='space',
+    exit_key='return'):
+    """
+    This is an automated version of the above 'manual'
+    calibration function. It is to be run the first time through -
+    any following recalibrations are handled manually, ie
+    by the experimenter.
+    """
+    # boolean indicating if calibration data are to be collected
+    # when target is shrunk to close to minimum size
+    collect_when_shrunk = False
+    # booleans indicating if shrink/grow sounds are allowed to be
+    # played (these are for avoiding sounds being repeatedly started
+    # in very quick succession)
+    allow_grow_sound = True
+    allow_shrink_sound = True
+    # boolean indicating if the target is currently growing
+    target_is_growing = False
+    # variable for holding previous frame's target width
+    previous_frame_width = None
+    # minimum possible target width
+    target_min_width = self.target_original_size[0] * self.calibration_target_min
+    # start calibration
+    event.clearEvents()
+    current_point_index = -1
+    in_calibration = True
+    clock = core.Clock()
+    # time at which the currently active target started being shown
+    # (the default value assigned here will be overwritten, see below)
+    target_start_time = clock.getTime()
+    # form a list of the target position numbers
+    pos_nums = self.numkey_dict.keys()
+    while in_calibration:
+        # if calibration hasn't already been set to trigger,
+        # and there are still points left to calibrate with
+        if not collect_when_shrunk and pos_nums:
+            # get the number of the position for which data will be fetched this time
+            pos_num = pos_nums.pop()
+            current_point_index = self.numkey_dict[key]
+            target_start_time = clock.getTime()
+            # trigger calibration data collection once target has
+            # shrunk enough
+            collect_when_shrunk = True
+        
+        # draw calibration target
+        if current_point_index in self.retry_points:
+            self.targets[current_point_index].setPos(
+                self.original_calibration_points[current_point_index])
+            t = (clock.getTime() - target_start_time) * self.shrink_speed
+            newsize = [(np.cos(t)**2 + self.calibration_target_min) * e
+                       for e in self.target_original_size]
+            self.targets[current_point_index].setSize(newsize)
+            self.targets[current_point_index].draw()
+            
+            # handle playing sounds if target started growing/shrinking
             if previous_frame_width is not None:
                 target_is_growing = previous_frame_width < newsize[0]
                 if target_is_growing and allow_grow_sound:
@@ -126,15 +250,21 @@ def customized_update_calibration(self,
                 current_point_index = -1
                 collect_when_shrunk = False
         self.win.flip()
-        
-        
+        # if calibration for current point is done, and there
+        # are no points left to calibrate for, end calibration
+        if not collect_when_shrunk and not pos_nums:
+            in_calibration = False
+
+
 
 
 # initialize TobiiInfantController to communicate with the eyetracker
 controller = TobiiInfantController(win)
 # use the customized calibration
-controller.update_calibration = types.MethodType(customized_update_calibration,
-                                                 controller)
+controller.update_calibration = types.MethodType(
+    main_calibration,
+    controller
+)
 
 # setup the attention grabber during adjusting the participant's position
 grabber.setAutoDraw(True)
